e2126c85969a87e0c1180e3aa28691a5dd759e57
Theodor Moroianu <theodor.moroianu@gmail.com>
2020-02-23 16:44:04 +0200

    Created tests for DFA class

--- LFA/Project2/Project2.cpp
+++ LFA/Project2/Project2.cpp
@@ -2,53 +2,59 @@
 #include "DFA.hpp"
 using namespace std;
 
-mt19937 rnd(0); 
+namespace Tester {
 
-void tester(DFA x, int sigma)
-{
-    DFA minim = x.Minimize();
-    
-    for (int _ = 0; _ < 10000; _++) {
-        string s;
-        int l = rnd() % 8;
-        for (int i = 0; i < l; i++)
-            s += 'a' + rnd() % sigma;
+    mt19937 rnd(time(0)); 
+
+    void tester(DFA x, int sigma)
+    {
+        DFA minim = x.Minimize();
         
-        if (x.IsAccepted(s) ^ minim.IsAccepted(s)) {
-            cerr << "Error for " << s << '\n';
-            cerr << "X.IsAccepted(" << s << ") = " << x.IsAccepted(s) << "\nBut minim.IsAccepted(" << s << ") = ";
-            cerr << minim.IsAccepted(s) << "!!\n";
-            cerr << "Initial DFA: " << x << '\n' << "Minimized DFA: " << minim << '\n';
-            exit(0);
+        for (int _ = 0; _ < 10000; _++) {
+            string s;
+            int l = rnd() % 8;
+            for (int i = 0; i < l; i++)
+                s += 'a' + rnd() % sigma;
+            
+            if (x.IsAccepted(s) ^ minim.IsAccepted(s)) {
+                cerr << "Error for " << s << '\n';
+                cerr << "X.IsAccepted(" << s << ") = " << x.IsAccepted(s) << "\nBut minim.IsAccepted(" << s << ") = ";
+                cerr << minim.IsAccepted(s) << "!!\n";
+                cerr << "Initial DFA: " << x << '\n' << "Minimized DFA: " << minim << '\n';
+                exit(0);
+            }
+        }
+        static int cnt = 0;
+        cerr << "Ok for " << cnt++ << '\n';
+    }
+
+    DFA make_random(int sigma)
+    {
+        int start_node = 0;
+        int l = rnd() % 15 + 1;
+        set <int> end_nodes = { (int)(rnd() % l), (int)(rnd() % l) };
+        vector <map <char, int>> edges(l);
+        for (int i = 0; i < 2 * l * l; i++) {
+            char c = rnd() % sigma + 'a';
+            edges[rnd() % l][c] = rnd() % l;
+        }
+
+        return DFA(edges, start_node, end_nodes);
+    }
+
+    void Tester() 
+    {
+        while (1) {
+            DFA r = make_random(4);
+            //cout << r << '\n';
+            tester(r, 4);
         }
     }
-    static int cnt = 0;
-    cerr << "Ok for " << cnt++ << '\n';
-}
-
-DFA make_random(int sigma)
-{
-    int start_node = 0;
-    int l = rnd() % 15 + 1;
-    set <int> end_nodes = { (int)(rnd() % l), (int)(rnd() % l) };
-    vector <map <char, int>> edges(l);
-    for (int i = 0; i < 2 * l * l; i++) {
-        char c = rnd() % sigma + 'a';
-        edges[rnd() % l][c] = rnd() % l;
-    }
-
-    return DFA(edges, start_node, end_nodes);
 }
 
 int main()
 {
-
-    while (1) {
-        DFA r = make_random(4);
-        //cout << r << '\n';
-        tester(r, 4);
-    }
-
+    Tester::Tester();
     ifstream in("input.txt");
 
     DFA a;
